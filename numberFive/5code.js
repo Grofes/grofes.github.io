f091cf799204c151a3f754fdd703bf03083df4ee
Grofes <92209055+Grofes@users.noreply.github.com>
2022-10-10 10:28:28 +0300

    Update 5code.js

--- numberFive/5code.js
+++ numberFive/5code.js
@@ -3,9 +3,9 @@ function raschitat() {
 cena  = document.getElementById('cena').value;
 kolvo  = document.getElementById('kolvo').value;
 if(cena == ""){
-alert("Вы не указали ширину");
+alert("Вы не указали цену");
 } else if(kolvo == ""){
-alert("Вы не указали длину");
+alert("Вы не указали кол-во");
 } else {
 stoimost = parseFloat (cena)* parseFloat (kolvo);
 document.getElementById('stoimost').innerHTML = "Стоимость равна: "+ stoimost +" р.";
